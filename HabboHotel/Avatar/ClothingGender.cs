73ec8e3130e4b0b8095f98a673a62975bf23f543
IDjinn <42049518+IDjinn@users.noreply.github.com>
2022-05-01 11:32:23 -0300

    ClothingGender enum

---
+++ HabboHotel/Avatar/ClothingGender.cs
@@ -0,0 +1,29 @@
+﻿namespace Plus.HabboHotel.Avatar;
+
+public enum ClothingGender
+{
+    None,
+    
+    Unisex,
+    Male,
+    Female
+}
+
+public static class ClothingGenderExtensions
+{
+    public static ClothingGender FromString(this string gender) => gender switch
+    {
+        "U" or "u" => ClothingGender.Unisex,
+        "M" or "m" => ClothingGender.Male,
+        "F" or "f" => ClothingGender.Female,
+        _ => ClothingGender.None
+    };
+
+    public static string ToString(this ClothingGender clothingGender) => clothingGender switch
+    {
+        ClothingGender.Female => "F",
+        ClothingGender.Male => "M",
+        ClothingGender.Unisex => "U",
+        _ => string.Empty
+    };
+}
