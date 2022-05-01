1cbe0a1da5937cf6107df60b7036c147178d675b
IDjinn <42049518+IDjinn@users.noreply.github.com>
2022-05-01 13:01:10 -0300

    invalid clothing gender exception

--- HabboHotel/Avatar/ClothingGender.cs
+++ HabboHotel/Avatar/ClothingGender.cs
@@ -1,9 +1,9 @@
-﻿namespace Plus.HabboHotel.Avatar;
+﻿using System;
+
+namespace Plus.HabboHotel.Avatar;
 
 public enum ClothingGender
 {
-    None,
-    
     Unisex,
     Male,
     Female
@@ -11,19 +11,18 @@ public enum ClothingGender
 
 public static class ClothingGenderExtensions
 {
-    public static ClothingGender FromString(this string gender) => gender switch
+    public static ClothingGender ParseFromString(this string gender) => gender switch
     {
         "U" or "u" => ClothingGender.Unisex,
         "M" or "m" => ClothingGender.Male,
         "F" or "f" => ClothingGender.Female,
-        _ => ClothingGender.None
+        _ => throw new ArgumentException("Invalid ClothingGender type.")
     };
 
     public static string ToString(this ClothingGender clothingGender) => clothingGender switch
     {
         ClothingGender.Female => "F",
         ClothingGender.Male => "M",
-        ClothingGender.Unisex => "U",
-        _ => string.Empty
+        _ => "U",
     };
 }
