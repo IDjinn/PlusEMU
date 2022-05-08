481da32bdd8f7bbb5750fb5740cfb724d0a211b8
IDjinn <42049518+IDjinn@users.noreply.github.com>
2022-05-08 14:43:27 -0300

    fix sql's

--- Resources/SQLs/Updates/3_DropIdKeyFromWardrobeTable.sql
+++ Resources/SQLs/Updates/3_DropIdKeyFromWardrobeTable.sql
@@ -1,7 +1,10 @@
-﻿
-ALTER TABLE `user_wardrobe`
-    DROP COLUMN `id`,
-    DROP INDEX `slot_id`,
-    DROP PRIMARY KEY,
-    DROP INDEX `user_id`,
-    ADD UNIQUE INDEX `user_id` (`user_id`, `slot_id`) USING BTREE;
+﻿CREATE TABLE `user_wardrobe` (
+    `user_id` INT(10) UNSIGNED NOT NULL,
+    `slot_id` INT(10) UNSIGNED NOT NULL,
+    `look` VARCHAR(120) NOT NULL COLLATE 'latin1_swedish_ci',
+     `gender` ENUM('F','M') NOT NULL DEFAULT 'M' COLLATE 'latin1_swedish_ci',
+     UNIQUE INDEX `user_id` (`user_id`, `slot_id`) USING BTREE
+)
+    COLLATE='latin1_swedish_ci'
+    ENGINE=InnoDB
+;
---
+++ Resources/SQLs/Updates/4_FigureManager.sql
@@ -0,0 +1,39 @@
+﻿
+CREATE TABLE `figure_sets` (
+    `id` INT(10) NOT NULL,
+    `type` VARCHAR(5) NOT NULL COLLATE 'utf8mb4_0900_ai_ci',
+    `palette_id` INT(10) NOT NULL,
+    `gender` ENUM('m','f','u') NOT NULL DEFAULT 'u' COLLATE 'utf8mb4_0900_ai_ci',
+    `club_level` INT(10) NOT NULL,
+     `colorable` TINYINT(1) NOT NULL DEFAULT '0',
+     `selectable` TINYINT(1) NOT NULL DEFAULT '0',
+     `pre_selectable` TINYINT(1) NOT NULL DEFAULT '0',
+     PRIMARY KEY (`id`) USING BTREE,
+     UNIQUE INDEX `type` (`type`, `palette_id`, `id`) USING BTREE
+)
+    COLLATE='utf8mb4_0900_ai_ci'
+    ENGINE=InnoDB
+;
+
+CREATE TABLE `figure_set_colors` (
+    `id` INT(10) NOT NULL,
+    `palette_id` INT(10) NOT NULL,
+    `index` INT(10) NOT NULL,
+    `club_level` INT(10) NOT NULL,
+    `selectable` TINYINT(1) NOT NULL DEFAULT '0',
+    `value` VARCHAR(10) NOT NULL COLLATE 'utf8mb4_0900_ai_ci',
+     PRIMARY KEY (`id`) USING BTREE
+)
+    COLLATE='utf8mb4_0900_ai_ci'
+    ENGINE=InnoDB
+;
+
+CREATE TABLE `figure_set_palettes` (
+    `id` INT(10) NOT NULL,
+    `color_id` INT(10) NOT NULL,
+    UNIQUE INDEX `id` (`id`, `color_id`) USING BTREE,
+    INDEX `color` (`color_id`) USING BTREE
+)
+    COLLATE='utf8mb4_0900_ai_ci'
+    ENGINE=InnoDB
+;
